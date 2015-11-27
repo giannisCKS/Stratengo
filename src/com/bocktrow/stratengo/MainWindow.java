8e681c81340bab9e5a9b0dfdfa8b700d2c4221a0
Bocktrow <spyrospagk@gmail.com>
2015-11-28 00:32:48 +0200

    Initial commit

---
+++ src/com/bocktrow/stratengo/MainWindow.java
@@ -0,0 +1,46 @@
+package com.bocktrow.stratengo;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+        super("Stratengo");
+        setSize(new Dimension(900, 900));
+        setResizable(false);
+        setLayout(new GridLayout(8, 10));
+
+        for (int i = 1; i <= 80; i++) {
+
+            ImageIcon icon = createImageIcon("asset/bluePieces/elfB.png", "elfB");
+            Image sized = icon.getImage().getScaledInstance(900 / 10, 900 / 8, Image.SCALE_SMOOTH);
+            ImageIcon resized = new ImageIcon(sized);
+            JButton jButton = new JButton("" + i);
+
+            if (i == 33 || i == 34 || i == 43 || i == 44 || i == 37 || i == 38 || i == 47 || i == 48) {
+                jButton.setEnabled(false);
+            } else {
+                jButton = new JButton(resized);
+            }
+
+            add(jButton);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new MainWindow().setVisible(true);
+    }
+
+    protected ImageIcon createImageIcon(String path, String description) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+}
+
